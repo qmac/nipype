1a093bec99d83abf84c252a50df8ab0693207bc5
adelavega <delavega@utexas.edu>
2017-03-29 16:55:35 -0400

    Supports multiple event files

--- nipype/interfaces/pliers.py
+++ nipype/interfaces/pliers.py
@@ -1,30 +1,33 @@
 from ..interfaces.base import (
     BaseInterfaceInputSpec, BaseInterface,
-    File, TraitedSpec, traits
+    File, TraitedSpec, traits, InputMultiPath, OutputMultiPath
 )
 from ..utils.misc import package_check
 
-import pandas as pd
-from os.path import split
+from os.path import split, abspath, basename
 
-have_pliers = True
-try:
-    package_check('pliers')
-except Exception as e:
-    have_pliers = False
-else:
-    from pliers.graph import Graph
-    from pliers.stimuli import load_stims
-    from pliers.export import to_long_format
+for package in ['pandas', 'pliers']:
+    try:
+        package_check(package)
+    except Exception as e:
+        raise ImportError(
+            "The package {} is required, but not installed".format(package))
+
+from pliers.graph import Graph
+from pliers.stimuli import load_stims
+from pliers.export import to_long_format
+import pandas as pd
 
 
 class PliersInterfaceInputSpec(BaseInterfaceInputSpec):
     graph_spec = traits.Dict(desc='a json spec for the graph')
-    events_file = File(exists=True, mandatory=True, desc='an events file')
-
+    event_files = InputMultiPath(File(exists=True), mandatory=True,
+                                 desc=('BIDS events.tsv file(s) containing onsets and durations '
+                                       'and regressors for each run.'))
 
 class PliersInterfaceOutputSpec(TraitedSpec):
-    events_file = File(exists=True, mandatory=True, desc='an events file')
+    event_files = OutputMultiPath(File,
+                                   desc=("Event files with pliers features added"))
 
 
 class PliersInterface(BaseInterface):
@@ -32,25 +35,30 @@ class PliersInterface(BaseInterface):
     output_spec = PliersInterfaceOutputSpec
 
     def _run_interface(self, runtime):
-        # Configure the stimuli
-        stims = []
-        event_df = pd.read_csv(self.inputs.events_file, sep='\t', na_values='n/a')
-        for event in event_df.iterrows():
-            stim = load_stims(event['stim_file'])
-            stim.onset = event['onset']
-            stim.duration = event['duration']
-            stims.append(stim)
+        output_event_files = []
+        for event_file in self.inputs.event_files:
+            # Configure the stimuli
+            stims = []
+            event_df = pd.read_csv(event_file, sep='\t', na_values='n/a')
+            for event in event_df.iterrows():
+                stim = load_stims(event['stim_file'])
+                stim.onset = event['onset']
+                stim.duration = event['duration']
+                stims.append(stim)
 
-        # Construct and run the graph
-        graph = Graph(self.inputs.graph_spec)
-        results = graph.run(stims)
+            # Construct and run the graph
+            graph = Graph(self.inputs.graph_spec)
+            results = graph.run(stims)
 
-        # Format and write the output
-        results = to_long_format(results)
-        event_df = pd.concat([event_df, results], axis=1)
-        output_events_file = split(self.inputs.events_file)[0] + '/pliers_events.tsv'
-        event_df.to_csv(output_events_file, sep='\t')
-        setattr(self, '_events_file', output_events_file)
+            # Format and write the output
+            results = to_long_format(results)
+            event_df = pd.concat([event_df, results], axis=1)
+            output_file = abspath(basename(event_file))
+            event_df.to_csv(output_file, sep='\t')
+
+            output_event_files.append(output_file)
+
+        setattr(self, '_event_files', output_event_files)
         return runtime
 
     def _list_outputs(self):
