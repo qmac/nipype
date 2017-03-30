8f4bc43ea4bc939bd12c339be68b674fbac8999c
adelavega <delavega@utexas.edu>
2017-03-30 15:19:14 -0400

    Pliers interface working decently

--- nipype/interfaces/pliers.py
+++ nipype/interfaces/pliers.py
@@ -1,10 +1,11 @@
 from ..interfaces.base import (
     BaseInterfaceInputSpec, BaseInterface,
-    File, TraitedSpec, traits, InputMultiPath, OutputMultiPath
+    File, TraitedSpec, InputMultiPath, OutputMultiPath,
+    Directory
 )
 from ..utils.misc import package_check
 
-from os.path import split, abspath, basename
+from os.path import abspath, basename, join
 
 for package in ['pandas', 'pliers']:
     try:
@@ -20,10 +21,12 @@ import pandas as pd
 
 
 class PliersInterfaceInputSpec(BaseInterfaceInputSpec):
-    graph_spec = traits.Dict(desc='a json spec for the graph')
+    graph_spec = File(exists=True, mandatory=True,
+                                  desc=("JSON specification of the features to etract."))
     event_files = InputMultiPath(File(exists=True), mandatory=True,
                                  desc=('BIDS events.tsv file(s) containing onsets and durations '
                                        'and regressors for each run.'))
+    bids_directory = Directory(exists=True, mandatory=True, desc=('BIDS directory'))
 
 class PliersInterfaceOutputSpec(TraitedSpec):
     event_files = OutputMultiPath(File,
@@ -35,26 +38,39 @@ class PliersInterface(BaseInterface):
     output_spec = PliersInterfaceOutputSpec
 
     def _run_interface(self, runtime):
+        stimuli_folder = join(self.inputs.bids_directory, 'stimuli')
+
+        # For each event file
         output_event_files = []
         for event_file in self.inputs.event_files:
-            # Configure the stimuli
             stims = []
             event_df = pd.read_csv(event_file, sep='\t', na_values='n/a')
-            for event in event_df.iterrows():
-                stim = load_stims(event['stim_file'])
-                stim.onset = event['onset']
-                stim.duration = event['duration']
-                stims.append(stim)
+            # For each stimulus
+            for i, event in event_df.iterrows():
+                stim_file = event['stim_file']
+                if pd.isnull(stim_file) is False:
+                    stim = load_stims(join(stimuli_folder, event['stim_file']))
+                    stim.onset = event['onset']
+                    stim.duration = event['duration']
+                    stim.name = event['stim_file']
+                    stims.append(stim)
 
             # Construct and run the graph
-            graph = Graph(self.inputs.graph_spec)
+            graph = Graph(spec=self.inputs.graph_spec)
             results = graph.run(stims)
 
             # Format and write the output
             results = to_long_format(results)
-            event_df = pd.concat([event_df, results], axis=1)
+
+            # Rename "stim" to "stim_file"
+            results = results.rename(columns={'stim' : 'stim_file',
+                                              'value' : 'extractor_value',
+                                              'feature' : 'extractor_feature'})
+
+            # Create and write files
+            event_df = pd.concat([event_df, results], axis=0)
             output_file = abspath(basename(event_file))
-            event_df.to_csv(output_file, sep='\t')
+            event_df.to_csv(output_file, sep='\t', index=False)
 
             output_event_files.append(output_file)
 
@@ -62,4 +78,4 @@ class PliersInterface(BaseInterface):
         return runtime
 
     def _list_outputs(self):
-        return {'events_file': getattr(self, '_events_file')}
+        return {'event_files': getattr(self, '_event_files')}
