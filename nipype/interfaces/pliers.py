dd76301fb25fc67d267a9855a3689b8ed9c14aaf
adelavega <delavega@utexas.edu>
2017-03-31 10:01:38 -0400

    Added subset filtering and event file querying

--- nipype/interfaces/pliers.py
+++ nipype/interfaces/pliers.py
@@ -1,6 +1,6 @@
 from ..interfaces.base import (
     BaseInterfaceInputSpec, BaseInterface,
-    File, TraitedSpec, InputMultiPath, OutputMultiPath,
+    File, TraitedSpec, InputMultiPath, OutputMultiPath, traits,
     Directory
 )
 from ..utils.misc import package_check
@@ -22,23 +22,28 @@ import pandas as pd
 
 class PliersInterfaceInputSpec(BaseInterfaceInputSpec):
     graph_spec = File(exists=True, mandatory=True,
-                                  desc=("JSON specification of the features to etract."))
-    event_files = InputMultiPath(File(exists=True), mandatory=True,
-                                 desc=('BIDS events.tsv file(s) containing onsets and durations '
-                                       'and regressors for each run.'))
-    bids_directory = Directory(exists=True, mandatory=True, desc=('BIDS directory'))
+                                  desc=("JSON specification of the features to extract."))
+    subset = traits.Dict(mandatory=False, desc=("Dictionary of entities on which to filter,"
+                                                "event files."))
+    bids_dir = Directory(exists=True, mandatory=True, desc=('A BIDS directory.'))
 
 class PliersInterfaceOutputSpec(TraitedSpec):
     event_files = OutputMultiPath(File,
                                    desc=("Event files with pliers features added"))
 
-
 class PliersInterface(BaseInterface):
     input_spec = PliersInterfaceInputSpec
     output_spec = PliersInterfaceOutputSpec
 
+    def _get_events(bids_dir, subset):
+        """ Get a subject's event files """
+        from bids.grabbids import BIDSLayout
+        layout = BIDSLayout(bids_dir)
+        events = layout.get(type='events', return_type='file', **subset)
+        return events
+
     def _run_interface(self, runtime):
-        stimuli_folder = join(self.inputs.bids_directory, 'stimuli')
+        stimuli_folder = join(self.inputs.bids_dir, 'stimuli')
 
         # For each event file
         output_event_files = []
@@ -63,12 +68,14 @@ class PliersInterface(BaseInterface):
             results = to_long_format(results)
 
             # Rename "stim" to "stim_file"
-            results = results.rename(columns={'stim' : 'stim_file',
-                                              'value' : 'extractor_value',
-                                              'feature' : 'extractor_feature'})
+            results = results.rename(columns={'stim': 'stim_file',
+                                              'value': 'extractor_value',
+                                              'feature': 'extractor_feature'})
 
             # Create and write files
             event_df = pd.concat([event_df, results], axis=0)
+
+            ### Maybe re-order
             output_file = abspath(basename(event_file))
             event_df.to_csv(output_file, sep='\t', index=False)
 
