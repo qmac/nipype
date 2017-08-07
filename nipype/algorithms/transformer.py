763f0263543b221059d85e26998195ddfb606f2a
adelavega <delavega@utexas.edu>
2017-08-07 18:02:46 -0500

    Adapted to new pybids transfor

--- nipype/algorithms/transformer.py
+++ nipype/algorithms/transformer.py
@@ -8,8 +8,11 @@ from ..utils.misc import package_check
 from nipype import logging
 iflogger = logging.getLogger('interface')
 
-from bids.transformer.base import BIDSEventCollection
-from bids.transformer.transform import BIDSTransformer
+from bids.events import BIDSEventCollection
+from bids.events import BIDSTransformer
+
+import os
+import glob
 
 try:
     package_check('pandas')
@@ -19,26 +22,43 @@ else:
     have_pandas = True
 
 class TransformEventsInputSpec(BaseInterfaceInputSpec):
-    bids_directory = Directory(exists=True, mandatory=True, desc=('BIDS directory'))
-    event_files = InputMultiPath(File(exists=True), mandatory=False,
-                                 desc=('Event files to use, instead of those in BIDS directory'))
-    transformation_spec = File(exists=True, mandatory=False,
-                                  desc=("JSON specification of the transformations to perform."))
-    ### Maybe some optional flags for how to select event files, if not provided? e.g. subjects?
-
-    ## Are the following necessary? Do we need to read form extractor_name rather than trial_type?
-    amplitude_column = traits.String(mandatory=False, requires='bids_events',
-                                     desc=("Column in events file to read amplitude from"))
-    condition_column = traits.String(mandatory=False, requires='bids_events',
-                                     desc=("Column in events file that codes conditions"))
-
+    bids_directory = Directory(
+        exists=True, mandatory=True,
+        desc=('Path to the root of the BIDS project.'))
+    event_files_directory = InputMultiPath(
+        File(exists=True), mandatory=False, xor=['filters'],
+        desc=('Directory containing event files to use, '
+              'instead of those in BIDS directory'))
+    filters = traits.Dict(
+        mandatory=False, xor=['event_files_directory'],
+        desc=("Additional entities to filter runs on."))
+    transformation_spec = File(
+        exists=True, mandatory=True,
+        desc=("Path to JSON specification of the transformations to perform."))
+    amplitude_column = traits.String(
+        mandatory=False,
+        desc=("Column providing the amplitudes that correspond to to the"
+              "'condition' column. Ignored if condition_column is None."))
+    condition_column = traits.String(
+        mandatory=False,
+        desc=("Column that contains the names of conditions. Defaults to"
+              "'trial_type', per the BIDS specification. If None, only extra"
+              "columns (beyond onset and duration) are inspected for events."))
+    default_duration = traits.Float(
+        mandatory=False,
+        desc=("Duration to assign when duration is missing."))
+    default_amplitude = traits.Float(
+        mandatory=False,
+        desc=("Default amplitude to assign to events in cases where an"
+              "amplitude is missing."))
+    time_repetition = traits.Float(
+        mandatory=True, desc=("Sampling rate to output event files."))
 
 
 class TransformEventsOutputSpec(TraitedSpec):
-    ## Or output as event files?
-    subject_info = OutputMultiPath(Bunch, mandatory=True,
-                                   desc=("Bunch or List(Bunch) subject specific condition information. "
-                                         "see :ref:`SpecifyModel` or SpecifyModel.__doc__ for details"))
+    event_files = OutputMultiPath(
+        Bunch, mandatory=True,
+        desc=("List of transformed event files"))
 
 
 class TransformEvents(BaseInterface):
@@ -48,20 +68,22 @@ class TransformEvents(BaseInterface):
 
     def _get_collection(self):
         """ Read in events """
-        if isdefined(self.inputs.event_files):
-            event_files = self.inputs.event_files
-        else:
-            event_files = None
-
+        # Init kwargs
         kwargs = {}
-        if isdefined(self.inputs.amplitude_column):
-            kwargs['amplitude_column'] = self.inputs.amplitude_column
-        if isdefined(self.inputs.condition_column):
-            kwargs['condition_column'] = self.inputs.condition_column
+        for arg in ['amplitude_column', 'condition_column', 'default_duration',
+                    'default_amplitude']:
+            attr = getattr(self.inputs, arg)
+            if isdefined(attr):
+                kwargs['arg'] = attr
+
+        files_directory = self.inputs.event_files \
+            if isdefined(self.inputs.event_files_directory) else None
+        filters = self.inputs.filters\
+            if isdefined(self.inputs.filters) else {}
 
         event_collection = BIDSEventCollection(
             base_dir=self.inputs.bids_directory, **kwargs)
-        event_collection.read(event_files)
+        event_collection.read(files_directory, **filters)
 
         return event_collection
 
@@ -70,10 +92,11 @@ class TransformEvents(BaseInterface):
 
         collection = self._get_collection()
 
-        transformer = BIDSTransformer(collection)
+        self._transformer = BIDSTransformer(collection)
 
         if isdefined(self.inputs.transformation_spec):
-            transformer.apply_from_json(self.inputs.transformation_spec)
+            self._transformer.apply_from_json(self.inputs.transformation_spec)
+
 
     def _run_interface(self, runtime):
         if not have_pandas:
@@ -82,15 +105,12 @@ class TransformEvents(BaseInterface):
         self._transform_events()
         return runtime
 
-    def _df_to_bunch(self):
-        ### Do we still want to do bunches?
-
-        ## Either way what is the output format?
-
-        ## Split by columns and runs? So it can be used in a MapNode?
-        pass
+    def _get_event_files(self):
+        cd = os.getcwd()
+        self.transformer.write(path=cd, sampling_rate=1./self.inputs.TR)
+        return glob.glob(os.path.join(os.getcwd(), '*_events.tsv'))
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['subject_info'] = self._df_to_bunch()
+        outputs['event_files'] = self._get_event_files()
         return outputs
